76e3adf72c4a13194934cd9d24cd80a2ebdf487c
Enrique Sanchez <enrike.sanchez@gmail.com>
2021-06-06 12:03:24 -0600

    answer 3

--- src/test/java/mx/tec/exam/stepdefinitions/BookStoreAPIStepDefs.java
+++ src/test/java/mx/tec/exam/stepdefinitions/BookStoreAPIStepDefs.java
@@ -33,6 +33,12 @@ public class BookStoreAPIStepDefs {
 	public void an_existing_user() {
 		// Do nothing
 	}
+	
+
+	@Given("an existing book")
+	public void an_existing_book() {
+		// Do nothing
+	}	
 
 	@When("the client calls \\/Account\\/v1\\/Authorized with username {string} and password {string}")
 	public void the_client_calls_account_v1_authorized_with_username_and_password(String username, String password) {
@@ -56,6 +62,14 @@ public class BookStoreAPIStepDefs {
 		
 		response = restTemplate.exchange("https://demoqa.com/Account/v1/GenerateToken",
 				HttpMethod.POST, entity, String.class);
+	}
+	
+	@When("the client calls \\/BookStore\\/v1\\/Book with ISBN {string}")
+	public void the_client_calls_book_store_v1_book_with_isbn(String isbn) {
+		HttpEntity<Map<String, String>> entity = new HttpEntity<Map<String, String>>(null, headers);
+		
+		response = restTemplate.exchange("https://demoqa.com/BookStore/v1/Book?ISBN=" + isbn,
+				HttpMethod.GET, entity, String.class);
 	}	
 
 	@Then("the client receives status code of {int}")
@@ -81,5 +95,12 @@ public class BookStoreAPIStepDefs {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode root = mapper.readTree(response.getBody());
 		assertEquals(value, root.path(property).asText());
-	}		
+	}
+	
+	@And("the book property {string} is {string}")
+	public void the_book_property_is(String property, String title) throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		assertEquals(title, root.path(property).asText());
+	}	
 }
--- src/test/resources/BookStoreAPI.feature
+++ src/test/resources/BookStoreAPI.feature
@@ -16,4 +16,10 @@ Feature: Book Store Web API
    	Given an existing user
     When the client calls /Account/v1/GenerateToken with username 'kikelink' and password 'wrong'
     Then the client receives status code of 200
-    And a 'token' property is 'null'    
+    And a 'token' property is 'null'
+    
+  Scenario: Find book by valid ISBN
+    Given an existing book
+    When the client calls /BookStore/v1/Book with ISBN '9781449325862'
+    Then the client receives status code of 200
+    And the book property 'title' is 'Git Pocket Guide'        
